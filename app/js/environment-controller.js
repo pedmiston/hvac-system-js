e1931eb10e193a3601c17dd1ff872a7063a2a3f6
Pierce Edmiston <pierce@8thlight.com>
2019-03-29 10:44:48 -0500

    Refactored tick to use state

--- app/js/environment-controller.js
+++ app/js/environment-controller.js
@@ -1,20 +1,32 @@
+/*jshint esversion: 6 */
+
 EnvironmentController = function(HVAC) {
+  // Possible states of the environment
   const states = {
     HEAT: "heat",
     COOL: "cool",
     OFF: "off"
-  }
+  };
+
+  // Determine the state of the environment
   this.getState = function() {
-    if(HVAC._temp < 65) state = states.HEAT
-    else if(HVAC._temp > 75) state = states.COOL
-    else state = states.OFF
-    return state
-  }
+    if(HVAC._temp < 65) { state = states.HEAT; }
+    else if(HVAC._temp > 75) { state = states.COOL; }
+    else { state = states.OFF; }
+    return state;
+  };
+
   this.tick = function() {
-    if(HVAC._temp >= 65 && HVAC._temp <= 75) {
-      HVAC.setFan(false);
-    } else {
-      HVAC.setFan(true);
+    switch (this.getState()) {
+      case states.HEAT:
+        HVAC.setFan(true);
+        break;
+      case states.COOL:
+        HVAC.setFan(true);
+        break;
+      case states.OFF:
+        HVAC.setFan(false);
+        break;
     }
   };
   return this;
